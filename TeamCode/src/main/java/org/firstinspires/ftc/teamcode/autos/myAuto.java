8ea88251c634aefbf921964da9e5fff587874f3f
kho25 <kho25@shschools.org>
2021-10-29 15:48:53 -0700

    added some functions to myAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/myAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/myAuto.java
@@ -2,12 +2,11 @@ package org.firstinspires.ftc.teamcode.autos;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.BaseRobot;
+import com.qualcomm.robotcore.hardware.*;
 
 
 public class myAuto extends LinearOpMode {
-    int stage = 0;
+
     /*
      * Steps:
      * 1. Deliver preloaded box to alliance shipping hub
@@ -21,6 +20,16 @@ public class myAuto extends LinearOpMode {
 
 
 
+
+    }
+
+    public void shippingHub(){
+
+    }
+    public void deliverDucks(){
+
+    }
+    public void park(){
 
     }
 }
