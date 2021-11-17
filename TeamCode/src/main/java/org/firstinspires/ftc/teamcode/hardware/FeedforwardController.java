25852d5c16bdad26a1cc253866786e336a385447
kho25 <91566656+kho25@users.noreply.github.com>
2021-11-17 15:41:43 -0800

    FeedforwardController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/FeedforwardController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/FeedforwardController.java
@@ -25,31 +25,51 @@ public class FeedforwardController {
         currentTime = new ElapsedTime();
         goal = goalPosition;
     }
-
-    public double[] updateValues(double currentPosition) {
+    public double getOutput (double currentPosition, MotionModel motionProfile) {
         double time = currentTime.seconds();
         double currentError = goal-currentPosition;
         double currentVelocity=(currentError-previousError)/(time-previousTime);
         double currentAcceleration = (currentVelocity-previousVelocity)/(time-previousTime);
+        double maximumSpeed;
+        double maxAcceleration;
+        double goalVelocity;
+        double goalAcceleration;
+        //start
+        
+        int direction_multiplier = 1;
+
+        if (currentError < 0) {
+            direction_multiplier = -1;
+        }
+        if (Math.abs(currentVelocity) < maximumSpeed){
+            K_v = currentVelocity + direction_multiplier * maxAcceleration * (time - previousTime);
+            K_a = maxAcceleration;
+        }
+   #if maximum speed has been reached, stay there for now
+   else:
+        outputVelocity = MAXIMUM_SPEED
+        outputAcceleration = 0
+
+   #if we are close enough to the object to begin slowing down
+        if position_error <= (output_velocity * output_velocity) / (2 * MAX_ACCELERATION)):
+        output_velocity = current_velocity - direction_multiplier * MAX_ACCELERATION * (current_time - previous_time)
+        output_acceleration = -MAX_ACCELERATION
+
+        previous_time = current_time
+                //end
+
+        double velError = motionProfile.getV(velocity)-velocity;
+        double accError = motionProfile.getA(acceleration)-acceleration;
+
+        output = F + k_V * velError + k_A * accError;
+
 
         previousError=currentError;
         previousTime=time;
         previousVelocity=currentVelocity;
         previousAcceleration=currentAcceleration;
 
-        return new double[]{currentVelocity, currentAcceleration};
-    }
-
-    public double getOutput (double currentPosition, MotionModel motionProfile) {
-        double[] temp = updateValues(currentPosition);
-        double velocity = temp[0];
-        double acceleration = temp[1];
-
-        //double velError = motionProfile.getV(velocity)-velocity;
-        //double accError = motionProfile.getA(acceleration)-acceleration;
-
-        //return F + k_V * velError + k_A * accError;
-        return 0;
+        return output;
     }
 
     public static class MotionModel {
