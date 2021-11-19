4546c584c062a59619ddb76c097760127ad9ec97
kho25 <91566656+kho25@users.noreply.github.com>
2021-11-19 15:22:26 -0800

    my auto 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/myAuto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/myAuto2.java
@@ -1,6 +1,13 @@
 package org.firstinspires.ftc.teamcode.autos;
 
 
+import static org.firstinspires.ftc.teamcode.hardware.ConstantVariables.K_D;
+import static org.firstinspires.ftc.teamcode.hardware.ConstantVariables.K_I;
+import static org.firstinspires.ftc.teamcode.hardware.ConstantVariables.K_P;
+import static org.firstinspires.ftc.teamcode.hardware.Devices.armLiftMotor1;
+import static org.firstinspires.ftc.teamcode.hardware.Devices.boxMover;
+import static org.firstinspires.ftc.teamcode.hardware.Devices.slideLiftMotor;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -10,10 +17,15 @@ import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
 import org.firstinspires.ftc.teamcode.hardware.ConstantVariables;
 import org.firstinspires.ftc.teamcode.hardware.Control;
 import org.firstinspires.ftc.teamcode.hardware.Devices;
+import org.firstinspires.ftc.teamcode.hardware.Encoders;
+import org.firstinspires.ftc.teamcode.hardware.SamplePipeline;
 
 @Autonomous
 
 public class myAuto2 extends LinearOpMode {
+
+    SamplePipeline pipeline;
+    int duckPositionIndex;
 
     public void runOpMode() {
         Devices.initDevices(hardwareMap);
@@ -27,8 +39,12 @@ public class myAuto2 extends LinearOpMode {
         if (isStopRequested()) return;
 
         Control.auto.moveWithEncoder(10, 0.5);
+        detectDucks();
         shippingHub();
+        placePreloaded();
+        carousel();
         deliverDucks();
+        park();
 
 
     }
@@ -37,9 +53,13 @@ public class myAuto2 extends LinearOpMode {
         Control.auto.moveWithEncoder(10, 0.5);
         Control.auto.turnWithGyro(45, -0.5);
         Control.auto.moveWithEncoder(10, 0.5);
+    }
+    public void carousel() {
         Control.auto.moveWithEncoder(10, -0.5);
         Control.auto.turnWithGyro(25, -0.5);
         Control.auto.moveWithEncoder(-23, 0.5);
+    }
+    public void park(){
         Control.auto.turnWithGyro(20, -0.5);
         Control.auto.moveWithEncoder(100, 0.5);
     }
@@ -57,5 +77,53 @@ public class myAuto2 extends LinearOpMode {
         }
     }
 
+    public void detectDucks() {
+
+        /*
+        Recognition duck = Control.auto.getDuck(telemetry);
+
+        double duckPositionAngle = duck.estimateAngleToObject(AngleUnit.DEGREES);
+        duckPositionIndex = Control.auto.getDuckPositionIndexThree(duckPositionAngle);
+         */
+
+
+        if (pipeline.inRegion1())
+            duckPositionIndex = 0;
+        else if (pipeline.inRegion2())
+            duckPositionIndex = 1;
+        else
+            duckPositionIndex = 3;
+    }
+
+    public void placePreloaded() {
+        double currentAngle = Control.auto.armEncoderToAngle(Encoders.getMotorEnc(armLiftMotor1));
+        double output;
+        int angle;
+        if (duckPositionIndex==0) angle = 0;
+        else if (duckPositionIndex==1) angle = 30;
+        else angle = 60;
+
+        Control.pid armController = new Control.pid();
+
+        boxMover.setPosition(1);
+
+        while (opModeIsActive() && currentAngle<angle-5){
+            currentAngle = Control.auto.armEncoderToAngle(Encoders.getMotorEnc(armLiftMotor1));
+            output = armController.rotateWithPid(angle, (currentAngle), K_P, K_I, K_D);
+            Control.motor.moveMotor(armLiftMotor1, output);
+            if (slideLiftMotor.getCurrentPosition()<100) slideLiftMotor.setPower(0.2);
+        }
+
+        ElapsedTime timer = new ElapsedTime();
+        telemetry.addLine("point reached");
+        boxMover.setPosition(0);
+
+        while (opModeIsActive()&&timer.seconds() < 1 ){
+            currentAngle = Control.auto.armEncoderToAngle(Encoders.getMotorEnc(armLiftMotor1));
+            output = armController.rotateWithPid(angle, (currentAngle), K_P, K_I, K_D);
+            Control.motor.moveMotor(armLiftMotor1, output);
+            if (slideLiftMotor.getCurrentPosition()<100) slideLiftMotor.setPower(0.2);
+        }
+    }
 
 }
